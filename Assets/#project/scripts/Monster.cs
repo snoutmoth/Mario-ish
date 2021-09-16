295ab0e7ef224bcf554dce948e4e9f25a34224ac
snoutmoth <mesdaria@gmail.com>
2021-09-16 16:43:30 +0200

    last day

--- Assets/#project/scripts/Monster.cs
+++ Assets/#project/scripts/Monster.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            LevelsManager.LoseLife();
+
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
--- Assets/#project/scripts/MovingMonster.cs
+++ Assets/#project/scripts/MovingMonster.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class MovingMonster : Monster
 {
     public Vector2 speed = Vector2.zero; //constante qui donne un vecteur (0,0)
 
     private SpriteRenderer spriteRenderer;
 
+    [Tooltip("if the monster detects an obstacle at a distance equal or less, they turn")]
     public float hitRange = 0.1f;
+
+    private Animator animator;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
 
     }
 
@@ -24,28 +30,49 @@ public class MovingMonster : Monster
         Vector2 start;
         Vector2 direction;
 
-        if(speed.x < 0) {                  //spriteRenderer.flipX = (speed.x < 0);
-            spriteRenderer.flipX = true;
+        if (speed.x < 0)
+        {
+            //si j'ai pas d'animation, il faut automatiquement un flip pour moi : les deux anim sont autonomes
+            if (animator != null)
+            {
+                animator.SetBool("Right", false);
+            }
+            else
+            {
+                spriteRenderer.flipX = true;
+            }
+
+            //spriteRenderer.flipX = (speed.x < 0);
+
             start = (Vector2)transform.position + Vector2.left * 0.51f;
             direction = Vector2.left;
         }
-        else {
-            spriteRenderer.flipX = false;
+        else
+        {
+            if (animator != null)
+            {
+                animator.SetBool("Right", true);
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
             start = (Vector2)transform.position + Vector2.right * 0.51f;
             direction = Vector2.right;
         }
         RaycastHit2D hit = Physics2D.Raycast(start, direction, hitRange); //permet de voir ce qu'il y a devant nous
         Debug.DrawRay(transform.position, Vector2.right, Color.red);
 
-        if(hit.collider != null) {
-            speed.x *= - 1; //s'il y a un Collider, on invesre la vitesse en X
+        if (hit.collider != null && !hit.transform.CompareTag("Player"))
+        {
+            speed.x *= -1; //s'il y a un Collider, on invesre la vitesse en X
         }
 
         //déplacement
         Vector2 deplacement = speed * Time.deltaTime;
         transform.position += (Vector3)deplacement; //casting : je veux caster déplacement en Vector3
-        
 
-        
+
+
     }
 }
